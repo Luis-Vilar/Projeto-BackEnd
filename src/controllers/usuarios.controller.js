f154b69c35d2c595852c79f72f8f5973252a9c7e
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2023-07-11 22:11:04 -0300

    :sparkles: feat : add login  de usuarios no endpoint /api/usuarios/login com o verbo post

--- src/controllers/usuarios.controller.js
+++ src/controllers/usuarios.controller.js
@@ -1,16 +1,18 @@
 const Usuarios = require("../models/Usuarios.js");
+const { config } = require("dotenv");
 const { estaNaBD } = require("../libs/validators.js");
-const { validarBody } = require("../libs/usuarios.lib.js");
-
+const { validarBody, informoEmailESenha } = require("../libs/usuarios.lib.js");
+const { sign } = require("jsonwebtoken");
+config();
 module.exports = {
   async store(req, res) {
     try {
+      const body = req.body;
       //validar se o body esta completo
-      if (!(await validarBody(req.body))) {
+      if (!(await validarBody(body))) {
         res.status(400);
         throw new Error("Requisição com dados inválidos");
       }
-      const body = req.body;
       //validar se o cpf ja esta cadastrado
       if (await estaNaBD(Usuarios, "cpf", body.cpf)) {
         res.status(409);
@@ -27,6 +29,50 @@ module.exports = {
         });
       }
       // caso algum erro ocorra devolvemos o erro para o cliente
+    } catch (error) {
+      return res.json({ message: error.message });
+    }
+  },
+  async login(req, res) {
+    try {
+      const body = req.body;
+      //validar se o body contem email e senha
+      if (!(await informoEmailESenha(body))) {
+        res.status(400);
+        throw new Error("Requisição com dados inválidos");
+      }
+      //existe cadastro com o email e senha informados?
+      const user = await Usuarios.findOne({
+        where: {
+          email: body.email,
+        },
+      });
+      //validar se o usuario existe
+      if (!user) {
+        res.status(404);
+        throw new Error("Usuário não encontrado");
+      }
+      //validar se o usuario esta ativo na base de dados
+      if (user.status === "inativo") {
+        res.status(401);
+        throw new Error("Seu usuario esta inativo");
+      }
+      //validar senha
+      if (body.senha === user.senha) {
+        //gerar payload
+        const payload = { status: user.status, id: user.id };
+        //gerar token
+        const token = sign(payload, process.env.JWT_KEY, {
+          expiresIn: "1d",
+        });
+        //retornar token
+        return res.status(200).json({
+          token,
+        });
+      } else {
+        res.status(401);
+        throw new Error("Falha na autenticação");
+      }
     } catch (error) {
       return res.json({ message: error.message });
     }
