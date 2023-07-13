6bba5fb87b315e88cb60baca1b732c1464c23481
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2023-07-13 19:02:35 -0300

    :sparkles: feat : add atualizar _depositos  no endpoint /api/depositos/:id  com o verbo patch

--- src/libs/depositos.lib.js
+++ src/libs/depositos.lib.js
@@ -38,7 +38,68 @@ async function validarBody(body) {
   }
   return true;
 }
+async function filtroUpdate(req, res) {
+  // pegamos o body da requisição
+  const body = req.body;
+  // so destruturamos os campos que queremos permitir atualizar
+  const {
+    nome_fantasia,
+    email,
+    telefone,
+    celular,
+    cep,
+    logradouro,
+    numero,
+    bairro,
+    cidade,
+    estado,
+  } = body;
+// criamos um objeto vazio
+  const novos_dados = {};
+// verificamos se o campo existe no body e se existe adicionamos ao objeto
+  if (nome_fantasia) {
+    novos_dados.nome_fantasia = nome_fantasia;
+  }
+  if (email) {
+    novos_dados.email = email;
+  }
+  if (telefone) {
+    novos_dados.telefone = telefone;
+  }
+  if (celular) {
+    novos_dados.celular = celular;
+  }
+  if (cep) {
+    novos_dados.cep = cep;
+  }
+  if (logradouro) {
+    novos_dados.logradouro = logradouro;
+  }
+  if (numero) {
+    novos_dados.numero = numero;
+  }
+  if (bairro) {
+    novos_dados.bairro = bairro;
+  }
+  if (cidade) {
+    novos_dados.cidade = cidade;
+  }
+  if (estado) {
+    novos_dados.estado = estado;
+  }
+  // se o objeto estiver vazio, não temos dados para
+  // atualizar ou informarem dados invalidos e não foi salvo no objeto
+  // então criamos um erro que sera capturado pelo try catch
+  if (Object.keys(novos_dados).length === 0) {
+    res.status(400);
+    throw new Error("Nenhum dado valido para atualizar");
+  }
+  // caso algunm dado valido esta em novos_dados retornamos o 
+  // objeto com os dados que queremos atualizar
+  return novos_dados;
+}
 
 module.exports = {
   validarBody,
+  filtroUpdate,
 };
--- src/routes/v1/depositos.js
+++ src/routes/v1/depositos.js
@@ -1,8 +1,9 @@
 const depositosRoutes = require("express").Router();
-const {store} = require('../../controllers/depositos.controller')
+const {store, update} = require('../../controllers/depositos.controller');
 const { validarToken } = require("../../middlewares/auth");
-
+//endpoinst protegidos por token
 depositosRoutes.post("/api/depositos",validarToken,store);
+depositosRoutes.patch("/api/depositos/:id",validarToken,update);
 
 
 module.exports = depositosRoutes;
