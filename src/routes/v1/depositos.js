407c8418d9226e00432361c61895f8d10a31138d
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2023-07-13 23:10:39 -0300

    :sparkles: feat : add listagem_depositos_por_id no endpoint /api/depositos/:id tambem se inclui dados do usuario criador do mesmo na response,  com o verbo get

--- src/routes/v1/depositos.js
+++ src/routes/v1/depositos.js
@@ -1,11 +1,12 @@
 const depositosRoutes = require("express").Router();
-const {store, update, status, index} = require('../../controllers/depositos.controller');
+const {store, update, status, index, indexId} = require('../../controllers/depositos.controller');
 const { validarToken } = require("../../middlewares/auth");
 //endpoints protegidos por token
 depositosRoutes.post("/api/depositos",validarToken,store);
 depositosRoutes.patch("/api/depositos/:id",validarToken,update);
 depositosRoutes.patch("/api/depositos/:id/status",validarToken,status)
 depositosRoutes.get("/api/depositos",validarToken,index)
+depositosRoutes.get("/api/depositos/:id",validarToken,indexId)
 
 
 module.exports = depositosRoutes;
